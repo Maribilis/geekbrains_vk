fad62584d0c2e519203bc31075fd8f91e2b9d056
maribilis <maribilis88@gmail.com>
2020-07-06 19:41:56 +0300

    сверстала логин и контроллер

---
+++ Zertsalova_VK/Zertsalova_VK/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Zertsalova_VK
+//
+//  Created by Natalia Zertsalova on 06.07.2020.
+//  Copyright © 2020 Natalia Zertsalova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewWillAppear(_ animated: Bool) {
+           super.viewWillAppear(animated)
+// создает нотификацию, когда клава показана
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+    }
+    @objc func keyboardWillShown(notification: Notification) {
+           let info = notification.userInfo! as NSDictionary
+           let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+
+         let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0)
+
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+
+}
+    @objc func keyboardWillHide(notification: Notification) {
+    scrollView.contentInset = UIEdgeInsets.zero
+    scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+}
+override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+}
+}
---
+++ Zertsalova_VK/Zertsalova_VKTests/Zertsalova_VKTests.swift
@@ -0,0 +1,34 @@
+//
+//  Zertsalova_VKTests.swift
+//  Zertsalova_VKTests
+//
+//  Created by Natalia Zertsalova on 06.07.2020.
+//  Copyright © 2020 Natalia Zertsalova. All rights reserved.
+//
+
+import XCTest
+@testable import Zertsalova_VK
+
+class Zertsalova_VKTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
